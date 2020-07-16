a54bae9814d1789241cd1b05c787d8bdd5b2759a
gergokutu <gergo.kutu@gmail.com>
2020-07-16 13:33:28 +0200

    Initialize Menu, Pokemons components

---
+++ src/components/Menu.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item to="/">Home</b-nav-item>
+          <b-nav-item to="/about">About</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default class Menu extends Vue {
+  
+};
+</script>
+
+<style lang="scss" scoped>
+#nav a.router-link-exact-active {
+    color: #ffffff;
+}
+</style>
