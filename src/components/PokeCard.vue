be3005ca6dd494b68b658f82d4dd55cf7a3c9493
gergokutu <gergo.kutu@gmail.com>
2020-07-17 10:43:11 +0200

    Implement card component from BpVue

---
+++ src/components/PokeCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Inside Pokemon component > Card...</h1>
+    <!-- <div class="card"> -->
+      <b-card
+        title="Card Title"
+        img-src="https://picsum.photos/600/300/?image=25"
+        img-alt="Image"
+        img-top
+        tag="article"
+        style="max-width: 20rem;"
+        class="mb-2"
+      >
+        <b-card-text>Some quick example text to build on the card title and make up the bulk of the card's content.</b-card-text>
+
+        <b-button href="#" variant="primary">Go somewhere</b-button>
+      </b-card>
+    </div>
+  <!-- </div> -->
+</template>
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component
+export default class PokeCard extends Vue {}
+</script>
--- src/components/Pokemons.vue
+++ src/components/Pokemons.vue
@@ -1,13 +1,22 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <PokeCard />
+    <PokeCard />
+    <PokeCard />
+    <PokeCard />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
+import PokeCard from '@/components/PokeCard.vue'
 
-@Component
+@Component({
+  components: {
+    PokeCard
+  }
+})
 export default class Pokemons extends Vue {
   @Prop() private msg!: string;
 }
