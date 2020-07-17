8cd7ac159d6d6b1896e933b359359f9d7580b1e6
gergokutu <gergo.kutu@gmail.com>
2020-07-17 10:20:27 +0200

    Create Categories view template

--- src/router/index.ts
+++ src/router/index.ts
@@ -14,6 +14,11 @@ Vue.use(VueRouter)
     path: '/random',
     name: 'Random',
     component: () => import('../views/Random.vue')
+  },
+  {
+    path: '/categories',
+    name: 'Categories',
+    component: () => import('../views/Categories.vue')
   }
 ]
 
