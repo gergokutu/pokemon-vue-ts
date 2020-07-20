c57839172f94a657a009d4e10c6c0b0ef7087c89
gergokutu <gergo.kutu@gmail.com>
2020-07-20 20:28:44 +0200

    Refactor IPokeCard interface to model.d.ts

--- src/interfaces/IPokeCard.ts
+++
@@ -1,4 +0,0 @@
-export default interface IPokeCard {
-  name: string;
-  url: string;
-}
--- src/store/modules/pokesModule.ts
+++ src/store/modules/pokesModule.ts
@@ -1,50 +1,51 @@
 import axios from 'axios'
 import { VuexModule, Module, Mutation, Action } from 'vuex-module-decorators'
-import IPokeCard from '@/interfaces/IPokeCard'
+import { IPokeCard } from './model'
 
 @Module({ namespaced: true, name: 'pokesModule' })
 class PokesModule extends VuexModule {
   public pokemons: Array<IPokeCard> = [
     {
-      "name": "unown1",
-      "url": "https://pokeapi.co/api/v2/pokemon/201/"
+      name: "unown1",
+      url: "https://pokeapi.co/api/v2/pokemon/201/"
     },
     {
-      "name": "wobbuffet",
-      "url": "https://pokeapi.co/api/v2/pokemon/202/"
+      name: "wobbuffet",
+      url: "https://pokeapi.co/api/v2/pokemon/202/"
     },
     {
-      "name": "unown2",
-      "url": "https://pokeapi.co/api/v2/pokemon/201/"
+      name: "unown2",
+      url: "https://pokeapi.co/api/v2/pokemon/201/"
     },
     {
-      "name": "unown3",
-      "url": "https://pokeapi.co/api/v2/pokemon/201/"
+      name: "unown3",
+      url: "https://pokeapi.co/api/v2/pokemon/201/"
     },
     {
-      "name": "unown4",
-      "url": "https://pokeapi.co/api/v2/pokemon/201/"
+      name: "unown4",
+      url: "https://pokeapi.co/api/v2/pokemon/201/"
     },
     {
-      "name": "unown5",
-      "url": "https://pokeapi.co/api/v2/pokemon/201/"
+      name: "unown5",
+      url: "https://pokeapi.co/api/v2/pokemon/201/"
     },
     {
-      "name": "unown6",
-      "url": "https://pokeapi.co/api/v2/pokemon/201/"
+      name: "unown6",
+      url: "https://pokeapi.co/api/v2/pokemon/201/"
     },
     {
-      "name": "unown7",
-      "url": "https://pokeapi.co/api/v2/pokemon/201/"
+      name: "unown7",
+      url: "https://pokeapi.co/api/v2/pokemon/201/"
     },
     {
-      "name": "unown8",
-      "url": "https://pokeapi.co/api/v2/pokemon/201/"
+      name: "unown8",
+      url: "https://pokeapi.co/api/v2/pokemon/201/"
     },
     {
-      "name": "unown9",
-      "url": "https://pokeapi.co/api/v2/pokemon/201/"
+      name: "unown9",
+      url: "https://pokeapi.co/api/v2/pokemon/201/"
     }
   ]
 }
+
 export default PokesModule
