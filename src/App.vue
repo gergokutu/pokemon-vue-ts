faae48f6416af593e5c98b371929d47fa37fd473
gergokutu <gergo.kutu@gmail.com>
2020-07-22 17:18:50 +0200

    Placeholder for Footer

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,22 @@
       <Menu />
     </div>
     <router-view/>
+    <div id="footer">
+      Footer...
+      <Footer />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import Menu from '@/components/Menu.vue'
+import Footer from '@/components/Footer.vue'
 
 @Component({
   components: {
-    Menu
+    Menu,
+    Footer
   }
 })
 export default class App extends Vue {
