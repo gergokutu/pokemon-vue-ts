ad0e302dbd70d2464b93ab89ba18ac0558dffcfc
gergokutu <gergo.kutu@gmail.com>
2020-07-22 15:44:11 +0200

    Show random pokemon details

--- src/views/Random.vue
+++ src/views/Random.vue
@@ -1,28 +1,71 @@
 <template>
-  <div class="about">
-    <h1>This page shows a random pokemon...</h1>
-    <PokeCard />
+<div class="about">
+  <h1>Random Pokemon</h1>
+  <div class="random-container">
+    <b-card
+      :img-src="details.data.sprites.front_default"
+      img-alt="Image"
+      img-top
+      tag="article"
+      style="min-width: 15rem;"
+      class="mb-2"
+    >
+      <b-card-text>
+        <p class="name">Name: {{ details.data.name }}</p>
+        <p>Base experience: {{ details.data.base_experience }}</p>
+        <p>Weight: {{ details.data.weight }}</p>
+        <p>Height: {{ details.data.height }}</p>
+      </b-card-text>
+    </b-card>
   </div>
+  <div class="back-button">
+      <b-button @click="refresh" variant="info">Back</b-button>
+    </div>
+</div>
+  
+  
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
-import PokeCard from '@/components/PokeCard.vue'
+import Details from '@/components/Details.vue'
+import { Detail } from '@/store/model'
 
 import { namespace } from 'vuex-class'
 const pokesModule = namespace('pokesModule')
 const detailsModule = namespace('detailsModule')
+const randomModule = namespace('randomModule')
 
 @Component({
   components: {
-    PokeCard
+    Details
   }
 })
 export default class Random extends Vue {
+  @randomModule.State
+  public details!: Detail
 
+  @randomModule.Action
+  public loadRandom!: () => void
+
+  created() {
+    this.loadRandom()
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+.random-container {
+  display: flex;
+  flex-wrap: wrap;
+  margin-right: 0px;
+  margin-left: 15%;
+  margin-top: 5%;
+  justify-content: center;
+  width: 70%;
+}
 
+.back-button {
+  margin-top: 10px;
+}
 </style>
