1c62ff41d1fc3295243aa32d2e162d2e43696258
gergokutu <gergo.kutu@gmail.com>
2020-07-22 17:09:22 +0200

    Wrong state...

---
+++ src/components/Footer.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <b-navbar toggleable="sm" type="dark" variant="info" fixed="top">
+      <b-navbar-brand href="#" class="brand">
+        Pokedex
+        <img src="@/assets/poke_icon.png" alt="Picachu" class="logo">
+      </b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="">
+          <b-nav-item to="/">Home</b-nav-item>
+          <b-nav-item to="/categories">Categories</b-nav-item>
+          <b-nav-item to="/random">Random</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-property-decorator'
+
+export default class Footer extends Vue {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
