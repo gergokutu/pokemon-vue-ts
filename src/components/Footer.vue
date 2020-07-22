2ce911cb5ca8e93e027e538185009602864f6f07
gergokutu <gergo.kutu@gmail.com>
2020-07-22 17:51:15 +0200

    Position footer to the bottoM > fixed

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="footer">
-    --- Made by gergokutu, 2020 ---
+    Footer @ Created by gergokutu, 2020
   </div>  
 </template>
 
 <style lang="scss">
 .footer {
   background-color: #17a2b8!important;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
   margin-top: 50px;
   height: 40px;
   padding: 10px;
