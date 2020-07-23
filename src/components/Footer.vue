144a84ca3006cc82d048ea75a9f38b32014df88d
gergokutu <gergo.kutu@gmail.com>
2020-07-23 16:54:11 +0200

    Import footer from BsVue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,17 +1,11 @@
 <template>
   <div class="footer">
-    Footer @ Created by gergokutu, 2020
-  </div>  
+    <b-navbar type="dark" variant="info" fixed="bottom">
+      By gergokutu, 2020
+    </b-navbar>
+  </div>
 </template>
 
 <style lang="scss">
-.footer {
-  background-color: #17a2b8!important;
-  position: fixed;
-  bottom: 0;
-  width: 100%;
-  margin-top: 50px;
-  height: 40px;
-  padding: 10px;
-}
+
 </style>
