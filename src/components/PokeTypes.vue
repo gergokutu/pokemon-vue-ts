e2a08d2a911bf43579fb3b31f6ab8e0c35b87398
gergokutu <gergo.kutu@gmail.com>
2020-08-01 01:21:05 +0200

    Fix Details page problem on > also call toggleShowDetails when click on type

--- src/components/PokeTypes.vue
+++ src/components/PokeTypes.vue
@@ -9,6 +9,7 @@ import { Vue, Component, Prop } from 'vue-property-decorator'
 
 import { namespace } from 'vuex-class'
 const categoriesModule = namespace('categoriesModule')
+const pokesModule = namespace('pokesModule')
 
 @Component
 export default class PokeTypes extends Vue {
@@ -21,8 +22,12 @@ export default class PokeTypes extends Vue {
   @categoriesModule.Action
   public loadNames!: (url: string) => void
 
+  @pokesModule.Mutation
+  public toggleShowDetails!: () => void
+
   private showNames(url: string): void {
     this.toggleShowNames()
+    this.toggleShowDetails()
     this.loadNames(url)
   }
 }
