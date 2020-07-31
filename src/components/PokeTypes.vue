987c5ea4b41fb1bf07550073b700b41127a2e2eb
gergokutu <gergo.kutu@gmail.com>
2020-08-01 01:30:42 +0200

    Remove toggleShowDetails from PokeTypes

--- src/components/PokeTypes.vue
+++ src/components/PokeTypes.vue
@@ -9,7 +9,6 @@ import { Vue, Component, Prop } from 'vue-property-decorator'
 
 import { namespace } from 'vuex-class'
 const categoriesModule = namespace('categoriesModule')
-const pokesModule = namespace('pokesModule')
 
 @Component
 export default class PokeTypes extends Vue {
@@ -22,12 +21,8 @@ export default class PokeTypes extends Vue {
   @categoriesModule.Action
   public loadNames!: (url: string) => void
 
-  @pokesModule.Mutation
-  public toggleShowDetails!: () => void
-
   private showNames(url: string): void {
     this.toggleShowNames()
-    this.toggleShowDetails()
     this.loadNames(url)
   }
 }
